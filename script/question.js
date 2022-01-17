6ec41265713ad51388ed4ac9dd60181221b7e1c2
renatadebertis <renata.debertis@gmail.com>
2022-01-17 17:33:03 +0000

    update new code

---
+++ script/question.js
@@ -0,0 +1,127 @@
+
+
+// Array of Objects
+const quiz = [
+    {
+        img:'img/chocolatefactory.jpg',
+        q:'Which child from "The Fantastic Chocolate Factory" found the first Golden Ticket?',
+        options:['Augustus Gloop','Veruca Salt','Violet Beauregarde','Mike Teavee'],
+        answer:0
+    },
+    {
+        img:'img/disney.jpg',
+        q:'Which of these Disney movies was released first?',
+        options:['Pocahontas','The Lion King','Mulan','Toy Story'],
+        answer:1
+    },
+    {
+        img:'img/panic.jpg',
+        q:'Which of these characters ended the film/franchise still alive?',
+        options:['Bill Murray ("Zombieland")','Dobby ("Harry Potter")',
+        'Jenny ("Forrest Gump")','Xerife Dewey Rilley ("Panic")'],
+        answer:3
+    },
+    {
+        img:'img/meryl.jpg',
+        q:'Which of these films did Meryl Streep NOT win an Oscar?',
+        options:['Kramer vs. Kramer','Doubt','The Iron Lady','The choice of sofia'],
+        answer:1
+    },
+    {
+        img:'img/it.jpg',
+        q:'Which movie made the most money at the domestic box office?',
+        options:['Star Wars: Episode IV - A new hope','Rebellious novice','Gone with the wind','E.T. the Extra-Terrestrial'],
+        answer:2
+    },
+    {
+        img:'img/psycho.jpg',
+        q:'What was the first movie to show a toilet flush?',
+        options:['What Ever Happened to Baby Jane?','Psycho','Casablanca','The Wizard of Oz'],
+        answer:1
+    },
+    {
+        img:'img/oscar.jpg',
+        q:'What was the first animated film to be nominated for an Academy Award for Best Picture?',
+        options:['Bambi','Little Mermaid','Spirited Away','Beauty and the Beast'],
+        answer:3
+    },
+    {
+        img:'img/13-Going-On-30.jpg',
+        q:'What song did Jenna Rink dance to in "13 Going on 30"?',
+        options:['I Wanna Dance with Somebody','Ice Ice Baby','Jessies Girls','Thriller'],
+        answer:3
+    },
+    {
+        img:'img/starwars.jpg',
+        q:'Who is the director of StarWars?',
+        options:['Roland Emerich','Steven Spilberg','George Lucas','J.J Abrams'],
+        answer:2
+    },
+    {
+        img:'img/Musical-Oscars.jpg',
+        q:'Which of these Oscar-nominated musicals won for Best Picture?',
+        options:['West Side Story','he Wizard of Oz','La La Land','Cabaret'],
+        answer:0
+    },
+    {
+        img:'img/Twin_Brothers.jpg',
+        q:'Who starred as Arnold Schwarzeneggers twin in the 1998 film "Twin Brothers"?',
+        options:['Eddie Murphy', 'Danny DeVito', 'Sylvester Stallone','Patrick Swayze'],
+        answer:1
+    },
+    {
+        img:'img/dwarfs.jpg',
+        q:'Which of these is not one of the Seven Dwarfs?',
+        options:['Blick','Flick','Quee','Click'],
+        answer:3
+    },
+    {
+        img:'img/starwars2.jpg',
+        q:'Who is not a StarWars character?',
+        options:['Kenobi','Spock','JarJar','Jango'],
+        answer:1
+    },
+    {
+        img:'img/Ellen_Ripley.jpg',
+        q:'The character Ellen Ripley is from what movie/franchise?',
+        options:['Alien','His Girl Friday','The Help','Avatar'],
+        answer:0
+    },
+    {
+        img:'img/eurovision.jpg',
+        q:'Where is the story of the Eurovision movie set?',
+        options:['Canada','Iceland','Norway','Russia'],
+        answer:1
+    },
+    {
+        img:'img/Luke_Skywalkers.jpg',
+        q:'What is the name of Luke Skywalkers best friend?',
+        options:['Chewbaca','Leia','Ioda','Hansolo'],
+        answer:3
+    },
+    {
+        img:'img/Ioda.jpg',
+        q:'How many years has Ioda been training jedals?',
+        options:['100','50','800','550'],
+        answer:2
+    },
+    {
+        img:'img/Harry_Potter.jpg',
+        q:'How long did the Harry Potter franchise last',
+        options:['5 years','7 years','10 years','15 years'],
+        answer:2
+    },
+    {
+        img:'img/Harry_Potter2.jpg',
+        q:'What is the last character to die in the latest Harry Potter franchise film',
+        options:['Harry Potter','Lord Voldemort','Severus Snape','Albus Dumbledore'],
+        answer:1
+    },
+    {
+        img:'img/The_Denial.jpg',
+        q:'Which actress played Deborah Lipstadt in the movie "The Denial"?',
+        options:['Rachel Wiesz','Caren Pistorius','Andrea Deck','Saily Messham'],
+        answer:0
+    },
+
+]
